dbec23506227b3d8b9a7a976bed5353ddf6eaca3
Alfred Lau <alfred@sulphur.com.sg>
2021-06-23 17:04:26 +0800

    :wrench: use `next-transpile-modules@latest` for next.js 11 support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-const withWPEHeadless = require('@wpengine/headless/nextConfig');
+function withWPEHeadless(nextConfig = {}) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-var-requires,global-require
+  const withTM = require('next-transpile-modules')(['@wpengine/headless']);
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
+  return withTM(nextConfig);
+}
 
 module.exports = withWPEHeadless();
