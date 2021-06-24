e09ac9c54d4d6ed6047f36ab65762f2018900237
Alfred Lau <alfred@sulphur.com.sg>
2021-06-25 00:08:00 +0800

    :art:

--- pages/preview/[[...page]].tsx
+++ pages/preview/[[...page]].tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  NextTemplateLoader,
-} from '@wpengine/headless/next';
+import { NextTemplateLoader } from '@wpengine/headless/next';
 
 import WPTemplates from '../../wp-templates/_loader';
 
@@ -10,5 +8,5 @@ import WPTemplates from '../../wp-templates/_loader';
  */
 export default function Page() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  return <NextTemplateLoader templates={ WPTemplates } />;
+  return <NextTemplateLoader templates={WPTemplates} />;
 }
