acf93f9c395eca5e4b68e3ae74ab447320d39272
Alfred Lau <alfred@sulphur.com.sg>
2021-06-24 23:24:31 +0800

    :lipstick: use `<Loader/>` component when page is loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import { AppContext, AppInitialProps } from 'next/app';
 import { HeadlessProvider } from '@wpengine/headless/react';
-import { motion } from 'framer-motion';
 import 'normalize.css/normalize.css';
 import 'scss/main.scss';
+import Loader from 'components/Loader';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function App({
@@ -15,22 +15,8 @@ export default function App({
   return (
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     <HeadlessProvider pageProps={pageProps}>
-      <motion.div
-        key={router.asPath}
-        initial="pageInitial"
-        animate="pageAnimate"
-        exit="pageInitial"
-        transition={{ duration: 1.2 }}
-        variants={{
-          pageInitial: {
-            opacity: 0,
-          },
-          pageAnimate: {
-            opacity: 1,
-          },
-        }}>
-        <Component {...pageProps} />
-      </motion.div>
+      <Loader />
+      <Component {...pageProps} />
     </HeadlessProvider>
   );
 }
