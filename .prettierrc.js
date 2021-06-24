37540e86e915acd1a1d91dc39c48833ff467218c
Alfred Lau <alfred@sulphur.com.sg>
2021-06-25 00:07:22 +0800

    :rewind:

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-	arrowParens: 'always',
-	jsxBracketSameLine: true,
-	singleQuote: true,
-	tabWidth: 2,
-	semi: true,
-	trailingComma: 'all',
-	endOfLine: 'lf',
-	useTabs: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  useTabs: false,
 };
