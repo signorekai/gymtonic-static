0a9e21e8f1952e3ef35701527e46141499eb9ed9
Alfred Lau <alfred@sulphur.com.sg>
2021-06-26 01:11:43 +0800

    :lipstick: avoid using state so that component doesn’t get re-rendered

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -11,31 +11,35 @@ const transition = { duration: 0.5, ease: [0.175, 0.85, 0.42, 0.96] };
 export default function Loader(): JSX.Element {
   const router = useRouter();
   const animControls = useAnimation();
-  const [active, setActive] = useState(true);
-
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { showLoader }: LoaderContextType = useContext(LoaderContext);
+  let { showLoader, setShowLoader }: LoaderContextType =
+    useContext(LoaderContext);
 
   useEffect(() => {
-    setActive(showLoader);
+    console.log('showLoader change', showLoader);
+    void animControls.stop();
+    void animControls.start({
+      opacity: showLoader ? 1 : 0,
+      transition,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showLoader]);
 
   const handleStart = (url: string) => {
-    if (url !== router.asPath) {
-      setActive(true);
-    }
+    console.log('start routechange');
+    void animControls.start({
+      opacity: 1,
+      transition,
+    });
   };
 
   const handleComplete = (url: string) => {
-    if (url !== '/') setActive(false);
-  };
-
-  useEffect(() => {
+    console.log('complete routechange');
     void animControls.start({
-      opacity: active ? 1 : 0,
+      opacity: 0,
       transition,
     });
-  }, [animControls, active]);
+  };
 
   useEffect(() => {
     router.events.on('routeChangeStart', handleStart);
@@ -47,14 +51,15 @@ export default function Loader(): JSX.Element {
       router.events.off('routeChangeComplete', handleComplete);
       router.events.off('routeChangeError', handleComplete);
     };
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <AnimatePresence>
       <motion.div
         animate={animControls}
         className="fixed z-50 top-0 left-0 w-full h-full pointer-events-none bg-pink flex flex-col justify-center items-center"
-        initial={{ opacity: 1 }}>
+        initial={{ opacity: showLoader ? 0 : 1 }}>
         <Image src={loading} alt="" width={126} height={222} />
       </motion.div>
     </AnimatePresence>
