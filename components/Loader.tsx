8a366ee35e1f16dc268d7a5c9e83e98a6feaefac
Alfred Lau <alfred@sulphur.com.sg>
2021-06-29 04:20:43 +0800

    :bug: <Loader /> now works properly

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,65 +1,62 @@
-import React, { useContext, useEffect, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { useRouter } from 'next/router';
-import { motion, AnimatePresence, useAnimation } from 'framer-motion';
 import Image from 'next/image';
+import { motion, AnimatePresence, useAnimation } from 'framer-motion';
+import { AnimationControls } from 'framer-motion/types/animation/types';
 
-import { LoaderContext, LoaderContextType } from 'pages/_app';
 import loading from '../assets/images/loading2.gif';
 
-const transition = { duration: 0.5, ease: [0.175, 0.85, 0.42, 0.96] };
+const transition = { duration: 0.35, ease: [0.175, 0.85, 0.42, 0.96] };
+// export const LoaderContext = createContext<any>(null);
 
-export default function Loader(): JSX.Element {
+interface Props {
+  showLoader: boolean;
+}
+
+export default function Loader({ showLoader = true }: Props): JSX.Element {
   const router = useRouter();
   const animControls = useAnimation();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  let { showLoader, setShowLoader }: LoaderContextType =
-    useContext(LoaderContext);
+  const refShowLoader = useRef(false);
+  const [showLoaderState, setShowLoaderState] = useState(refShowLoader.current);
 
   useEffect(() => {
-    console.log('showLoader change', showLoader);
-    void animControls.stop();
-    void animControls.start({
-      opacity: showLoader ? 1 : 0,
-      transition,
-    });
+    async function changeLoaderState() {
+      if (showLoader !== refShowLoader.current) {
+        console.log('Show <motion.div>, initial opacity:', {
+          opacity: refShowLoader.current ? 1 : 0,
+        });
+        setShowLoaderState(showLoader);
+        refShowLoader.current = showLoader;
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        console.log(`animating loader to ${showLoader ? 1 : 0}`);
+        await animControls.start({
+          opacity: showLoader ? 1 : 0,
+          transition,
+        });
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        console.log(`animated loader to ${showLoader === true}`);
+        // update state after this is done
+      }
+    }
+    void changeLoaderState();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showLoader]);
 
-  const handleStart = (url: string) => {
-    console.log('start routechange');
-    void animControls.start({
-      opacity: 1,
-      transition,
-    });
-  };
-
-  const handleComplete = (url: string) => {
-    console.log('complete routechange');
-    void animControls.start({
-      opacity: 0,
-      transition,
-    });
-  };
-
-  useEffect(() => {
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleComplete);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter initial={false}>
       <motion.div
+        key={router.asPath}
+        initial={{ opacity: refShowLoader.current ? 1 : 0 }}
         animate={animControls}
-        className="fixed z-50 top-0 left-0 w-full h-full pointer-events-none bg-pink flex flex-col justify-center items-center"
-        initial={{ opacity: showLoader ? 0 : 1 }}>
+        transition={transition}
+        className="fixed z-50 top-0 left-0 w-full h-full pointer-events-none bg-pink flex flex-col justify-center items-center">
         <Image src={loading} alt="" width={126} height={222} />
       </motion.div>
     </AnimatePresence>
