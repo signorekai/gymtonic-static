6ad319a646936db34bc6838c67690d509d57db1e
Alfred Lau <alfred@sulphur.com.sg>
2021-07-05 01:02:56 +0800

    :wrench: /story/:slug goes to the same component

--- wp-templates/_loader.tsx
+++ wp-templates/_loader.tsx
@@ -6,6 +6,7 @@ const templates: Templates<NextTemplate> = {
   '404': import('./404'),
   'front-page': import('./front-page'),
   'page-stories': import('./page-stories'),
+  'single-story': import('./page-stories'),
   index: import('./index'),
   category: import('./category'),
   page: import('./page'),
