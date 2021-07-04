5edd8c95b56c4f89d40bd7d7341144a71e3ea0fd
Alfred Lau <alfred@sulphur.com.sg>
2021-07-05 03:32:13 +0800

    :wrench: added page-about-proven template to loader

--- wp-templates/_loader.tsx
+++ wp-templates/_loader.tsx
@@ -6,6 +6,7 @@ const templates: Templates<NextTemplate> = {
   '404': import('./404'),
   'front-page': import('./front-page'),
   'page-about': import('./page-about'),
+  'page-about-proven': import('./page-about-proven'),
   'page-stories': import('./page-stories'),
   'single-story': import('./page-stories'),
   index: import('./index'),
