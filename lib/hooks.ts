60dcbe50f04180d0b01a318ad3daf3047761d9c4
Alfred Lau <alfred@sulphur.com.sg>
2021-07-07 02:56:39 +0800

    :sparkles: added delta to useScroll()

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -8,12 +8,21 @@ export function useScroll(
 ): {
   scrollX: number;
   scrollY: number;
+  deltaX: number;
+  deltaY: number;
   scrollXProgress: number;
   scrollYProgress: number;
 } {
+  const previousValues = useRef({
+    scrollX: 0,
+    scrollY: 0,
+  });
+
   const values = useRef({
     scrollX: 0,
     scrollY: 0,
+    deltaX: 0,
+    deltaY: 0,
     scrollXProgress: 0,
     scrollYProgress: 0,
   });
@@ -31,21 +40,33 @@ export function useScroll(
         values.current.scrollX = window.pageXOffset - element?.offsetLeft;
         values.current.scrollY = window.pageYOffset - element?.offsetTop;
 
+        values.current.deltaX =
+          values.current.scrollX - previousValues.current.scrollX;
+
+        values.current.deltaY =
+          values.current.scrollY - previousValues.current.scrollY;
+
+        previousValues.current.scrollX = values.current.scrollX;
+        previousValues.current.scrollY = values.current.scrollY;
+
         if (options.canGoBeyondBoundary === false) {
           if (values.current.scrollX < 0) values.current.scrollX = 0;
           if (values.current.scrollY < 0) values.current.scrollY = 0;
         }
 
         values.current.scrollXProgress =
-          values.current.scrollX / (element.scrollWidth - window.innerWidth);
+          values.current.scrollX / (element.scrollWidth - window.innerWidth) ||
+          0;
         values.current.scrollYProgress =
-          values.current.scrollY / (element.scrollHeight - window.innerHeight);
+          values.current.scrollY /
+            (element.scrollHeight - window.innerHeight) || 0;
 
         if (options.canGoBeyondBoundary === false) {
           values.current.scrollXProgress = Math.max(
             0,
             values.current.scrollXProgress,
           );
+
           values.current.scrollXProgress = Math.min(
             1,
             values.current.scrollXProgress,
