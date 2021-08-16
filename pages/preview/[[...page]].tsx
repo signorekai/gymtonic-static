f1f80779ec282871112d8d1bbb26bceb01052236
Alfred Lau <alfred@sulphur.com.sg>
2021-08-16 22:28:47 +0800

    :construction: preview page

--- pages/preview/[[...page]].tsx
+++ pages/preview/[[...page]].tsx
@@ -1,7 +1,34 @@
 import React from 'react';
-import { NextTemplateLoader } from '@wpengine/headless/next';
-
+import {
+  NextTemplateLoader,
+  getNextStaticPaths,
+  getNextStaticProps,
+} from '@wpengine/headless/next';
+import { getApolloClient } from '@wpengine/headless';
+import { gql } from '@apollo/client';
+import { GetStaticPropsContext } from 'next';
 import WPTemplates from '../../wp-templates/_templates';
+
+export const menuQuery = gql`
+  {
+    menu(id: "dGVybToy") {
+      id
+      menuItems {
+        edges {
+          cursor
+          node {
+            cssClasses
+            order
+            url
+            label
+            path
+            id
+          }
+        }
+      }
+    }
+  }
+`;
 
 /**
  * @todo make conditionalTags available
@@ -9,4 +36,18 @@ import WPTemplates from '../../wp-templates/_templates';
 export default function Page() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   return <NextTemplateLoader templates={WPTemplates} />;
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const client = getApolloClient(context);
+  await client.query({
+    query: menuQuery,
+  });
+  return getNextStaticProps(context, {
+    templates: WPTemplates,
+  });
+}
+
+export function getStaticPaths() {
+  return getNextStaticPaths();
 }
