2af27e5e4943f5b6e954c412cade7d6731618c71
Alfred Lau <alfred@sulphur.com.sg>
2021-08-24 18:03:06 +0800

    :pencil2:

--- wp-templates/page-locations.tsx
+++ wp-templates/page-locations.tsx
@@ -526,7 +526,7 @@ const Page: React.FunctionComponent<any> = ({
                 key="show-list">
                 <header className="pointer-events-none top-0 w-full pt-12 lg:pt-20 text-center z-30">
                   <h1 className="page-title relative z-20 pt-2 pb-4 block">
-                    Where to find us
+                    Locations
                   </h1>
                   <motion.h2
                     variants={{
