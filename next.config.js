0a70b5dc0b631f08b3c76bec27dba7f934695e09
Alfred Lau <alfred@sulphur.com.sg>
2021-11-01 21:19:08 +0800

    :wrench:

--- next.config.js
+++ next.config.js
@@ -13,4 +13,19 @@ module.exports = withWPEHeadless({
     deviceSizes: [360, 375, 414, 768, 1024, 1366, 1600],
     imageSizes: [64, 96, 128],
   },
+  // https://stackoverflow.com/questions/68565169/using-tailwind-arbitrary-value-support-with-scss/68959514#68959514
+  webpack(config) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (moduleLoader.loader.includes('resolve-url-loader'))
+          moduleLoader.options.sourceMap = false;
+      });
+    });
+
+    return config;
+  },
 });
