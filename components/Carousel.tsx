30db29b17b781312a71bc488b48bb9ae6a8c91a1
Alfred Lau <alfred@sulphur.com.sg>
2021-11-05 00:06:07 +0800

    :lipstick: figured out how to properly do full height

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -9,6 +9,7 @@ interface CarouselProps {
   leftNavBtnStyle?: React.CSSProperties;
   rightNavBtnStyle?: React.CSSProperties;
   navBtnPosition?: 'top' | 'center';
+  repeat?: number;
 }
 
 interface CarouselCardProps {
@@ -27,7 +28,7 @@ export const CarouselCard: React.FunctionComponent<CarouselCardProps> = ({
   return (
     <motion.article
       variants={{ show: { opacity: 1 }, hide: { opacity: 1 } }}
-      className={`flex-1 h-full px-0 ${className}`}>
+      className={`flex-1 h-full px-0 flex flex-col justify-between ${className}`}>
       <AnimatePresence exitBeforeEnter>
         {index === currentIndex && children}
       </AnimatePresence>
@@ -43,35 +44,20 @@ const Carousel: React.FunctionComponent<CarouselProps> = ({
   navBtnStyle = {},
   rightNavBtnStyle = {},
   leftNavBtnStyle = {},
+  repeat = 10,
 }: CarouselProps) => {
-  const [currentIndex, setCurrentIndex] = useState(children.length * 10);
+  const [currentIndex, setCurrentIndex] = useState(children.length * repeat);
   const container = useRef<HTMLDivElement>(null);
   const slidesContainer = useRef<HTMLDivElement>(null);
   const carouselAnimationControls = useAnimation();
 
-  const infiniteChildren = [
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-    ...children,
-  ];
+  const totalIteration = repeat * 2 + 1;
+  const infiniteChildren = [];
+
+  // eslint-disable-next-line no-plusplus
+  for (let x = 0; x < totalIteration; x++) {
+    infiniteChildren.push(...children);
+  }
 
   const fullNavBtnStyle = {
     display: 'inline-block',
@@ -101,120 +87,118 @@ const Carousel: React.FunctionComponent<CarouselProps> = ({
   );
 
   return (
-    <div className={`max-w-full flex-1 relative ${className}`}>
-      <div
-        className={`flex flex-row h-full ${
-          navBtnPosition === 'top' ? 'items-start' : 'items-center'
-        }`}>
-        {showNav && (
-          <button
-            type="button"
-            className={`inline-block ml-2 md:ml-9 transition-opacity duration-200 p-3 text-white ${
-              currentIndex === 0 ? 'opacity-50 hover:cursor-not-allowed' : ''
-            }`}
-            style={{ ...fullNavBtnStyle, ...leftNavBtnStyle }}
-            onClick={() => {
-              goTo(-1);
-            }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="28"
-              height="28"
-              viewBox="0 0 28 28">
+    <div
+      className={`max-w-full flex flex-row flex-1 h-full ${className} ${
+        navBtnPosition === 'top' ? 'items-start' : 'items-center'
+      }`}>
+      {showNav && (
+        <button
+          type="button"
+          className={`inline-block ml-2 md:ml-9 transition-opacity duration-200 p-3 text-white ${
+            currentIndex === 0 ? 'opacity-50 hover:cursor-not-allowed' : ''
+          }`}
+          style={{ ...fullNavBtnStyle, ...leftNavBtnStyle }}
+          onClick={() => {
+            goTo(-1);
+          }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            viewBox="0 0 28 28">
+            <g
+              transform="translate(-1646 -2601) rotate(90)"
+              className="stroke-current">
               <g
-                transform="translate(-1646 -2601) rotate(90)"
-                className="stroke-current">
-                <g
-                  transform="translate(2629 -1674) rotate(90)"
-                  fill="none"
-                  strokeWidth="1.5">
-                  <circle cx="14" cy="14" r="14" stroke="none" />
-                  <circle cx="14" cy="14" r="13.25" fill="none" />
-                </g>
-                <path
-                  d="M4315.6,3241.3l6.374,5.975-6.355,6.018"
-                  transform="translate(5862.298 -5977.787) rotate(90)"
-                  fill="none"
-                  strokeWidth="1.5"
-                />
+                transform="translate(2629 -1674) rotate(90)"
+                fill="none"
+                strokeWidth="1.5">
+                <circle cx="14" cy="14" r="14" stroke="none" />
+                <circle cx="14" cy="14" r="13.25" fill="none" />
               </g>
-            </svg>
-          </button>
-        )}
-        <div className="flex-1 h-full overflow-hidden" ref={container}>
-          <motion.div
-            ref={slidesContainer}
-            initial="initial"
-            exit="exit"
-            animate="enter"
-            variants={{
-              initial: (custom: {
-                currentIndex: number;
-                childrenCount: number;
-              }) => ({
-                x: `${(custom.currentIndex / custom.childrenCount) * -100}%`,
-              }),
-              enter: (custom: {
-                currentIndex: number;
-                childrenCount: number;
-              }) => ({
-                x: `${(custom.currentIndex / custom.childrenCount) * -100}%`,
-                transition: {
-                  duration: 0.2,
-                },
-              }),
-            }}
-            custom={{
-              currentIndex,
-              childrenCount: infiniteChildren.length,
-            }}
-            className="flex flex-row flex-wrap md:px-0"
-            style={{
-              width: `${infiniteChildren.length * 100}%`,
-            }}>
-            {childrenWithProps}
-          </motion.div>
-        </div>
-        {showNav && (
-          <button
-            type="button"
-            className={`inline-block mr-2 md:mr-9 transition-opacity p-3 duration-200 ${
-              currentIndex === infiniteChildren.length - 1
-                ? 'opacity-50 hover:cursor-not-allowed'
-                : ''
-            }`}
-            style={{ ...fullNavBtnStyle, ...rightNavBtnStyle }}
-            onClick={() => {
-              goTo(1);
-            }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="28"
-              height="28"
-              viewBox="0 0 28 28">
-              <g
-                transform="translate(1674 2629) rotate(-90)"
-                className="stroke-current">
-                <g
-                  id="Ellipse_417"
-                  data-name="Ellipse 417"
-                  transform="translate(2629 -1674) rotate(90)"
-                  fill="none"
-                  strokeWidth="1.5">
-                  <circle cx="14" cy="14" r="14" stroke="none" />
-                  <circle cx="14" cy="14" r="13.25" fill="none" />
-                </g>
-                <path
-                  d="M4315.6,3241.3l6.374,5.975-6.355,6.018"
-                  transform="translate(5862.298 -5977.787) rotate(90)"
-                  fill="none"
-                  strokeWidth="1.5"
-                />
-              </g>
-            </svg>
-          </button>
-        )}
+              <path
+                d="M4315.6,3241.3l6.374,5.975-6.355,6.018"
+                transform="translate(5862.298 -5977.787) rotate(90)"
+                fill="none"
+                strokeWidth="1.5"
+              />
+            </g>
+          </svg>
+        </button>
+      )}
+      <div className="overflow-hidden flex-1 self-stretch" ref={container}>
+        <motion.div
+          ref={slidesContainer}
+          initial="initial"
+          exit="exit"
+          animate="enter"
+          variants={{
+            initial: (custom: {
+              currentIndex: number;
+              childrenCount: number;
+            }) => ({
+              x: `${(custom.currentIndex / custom.childrenCount) * -100}%`,
+            }),
+            enter: (custom: {
+              currentIndex: number;
+              childrenCount: number;
+            }) => ({
+              x: `${(custom.currentIndex / custom.childrenCount) * -100}%`,
+              transition: {
+                duration: 0.1,
+              },
+            }),
+          }}
+          custom={{
+            currentIndex,
+            childrenCount: infiniteChildren.length,
+          }}
+          className="flex flex-row flex-wrap md:px-0 h-full"
+          style={{
+            width: `${infiniteChildren.length * 100}%`,
+          }}>
+          {childrenWithProps}
+        </motion.div>
       </div>
+      {showNav && (
+        <button
+          type="button"
+          className={`inline-block mr-2 md:mr-9 transition-opacity p-3 duration-200 ${
+            currentIndex === infiniteChildren.length - 1
+              ? 'opacity-50 hover:cursor-not-allowed'
+              : ''
+          }`}
+          style={{ ...fullNavBtnStyle, ...rightNavBtnStyle }}
+          onClick={() => {
+            goTo(1);
+          }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            viewBox="0 0 28 28">
+            <g
+              transform="translate(1674 2629) rotate(-90)"
+              className="stroke-current">
+              <g
+                id="Ellipse_417"
+                data-name="Ellipse 417"
+                transform="translate(2629 -1674) rotate(90)"
+                fill="none"
+                strokeWidth="1.5">
+                <circle cx="14" cy="14" r="14" stroke="none" />
+                <circle cx="14" cy="14" r="13.25" fill="none" />
+              </g>
+              <path
+                d="M4315.6,3241.3l6.374,5.975-6.355,6.018"
+                transform="translate(5862.298 -5977.787) rotate(90)"
+                fill="none"
+                strokeWidth="1.5"
+              />
+            </g>
+          </svg>
+        </button>
+      )}
     </div>
   );
 };
