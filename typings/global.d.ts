dfaabdb3d1e2abf08b1b68f712eab334dc31f602
Alfred Lau <alfred@sulphur.com.sg>
2022-02-14 15:11:43 +0800

    :label: added type support for process.env

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -150,3 +150,18 @@ interface IActiveInfoWindow {
   visible: boolean;
   style: 'red' | 'white';
 }
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      CONTACT_EMAIL: string;
+      CONTACT_EMAIL_PASS: string;
+      CONTACT_EMAIL_HOST: string;
+      CONTACT_EMAIL_PORT: number;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
