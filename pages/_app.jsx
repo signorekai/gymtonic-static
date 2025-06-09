204d9abacdacf18efe7e5d3a274dc9d46e9546ca
Alfred Lau <signorekai@gmail.com>
2025-06-09 22:15:01 +0800

    add buildtime into console

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import { HeadlessProvider } from '@wpengine/headless/react';
 import '../scss/main.scss';
 
 export default function App({ Component, pageProps }) {
+  console.log(`BUILT AT : ${process.env.PUBLIC_BUILD_TIME_START}`)
   return (
     <HeadlessProvider pageProps={pageProps}>
       <Component {...pageProps} />
